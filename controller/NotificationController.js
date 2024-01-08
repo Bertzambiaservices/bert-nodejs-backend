a0231e78a474060601e9cc3083f1bd7b9592d89a
Albert354 <132961821+Albert354@users.noreply.github.com>
2024-01-07 22:08:36 -0500

    first commit

--- controller/NotificationController.js
+++ controller/NotificationController.js
@@ -5,7 +5,7 @@ const notify = async (req, res) => {
     var fcm = new FCM(fcm_key);
 
     var message = {
-    to:'BIFxZL0jfb3CS7mh85o2rzVDYG0wh4I0GoGy9aFOHo2SJDOw32S9fO6E2y5h-niINm2yPzaXggmAvb3xIz1oyoU',
+    to:'notify',
     Notification:{
         title:"title 1 demo",
         body:"body 1 demo"
